8c587b7631cc85043d7d65a557a32bfdf646c93c
root <skkasans@gmail.com>
2020-11-01 07:05:32 +0000

    add arrary add header

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,41 @@
 <template>
   <div>
     <h1>Todoリスト</h1>
+    <table>
+      <tr>
+        <th>内容</th>
+        <th>完了</th>
+        <th>追加日</th>
+      </tr>
+
+    </table>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          title: '買い物',
+          done: false,
+          created_at: new Date()
+        },
+        {
+          title: 'Jsの勉強',
+          done: false,
+          created_at: new Date()
+        },
+      ]
+    }
+  }
+
+}
 </script>
 
 <style>
-
+th, td {
+  border: 1px solid #ddd;
+}
 </style>
